b61c8d61e210c9d1cb444fe8755c04a7092412d5
ajaykumaar <31564988+ajaykumaar@users.noreply.github.com>
2019-07-10 18:16:25 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=16FHNOTuPYv3XCLj3ZcoMe5U264K5mBY_'
+export_file_name = 'expression_classifier.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = [ 'angry','sad', 'shock', 'smiling']
 path = Path(__file__).parent
 
 app = Starlette()
